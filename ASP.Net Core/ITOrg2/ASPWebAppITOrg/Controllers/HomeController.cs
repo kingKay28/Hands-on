6da44d8dd4b1ca21f7d4650e5aa281d5d14ff696
Kingshuk Pal <44269603+kingKay28@users.noreply.github.com>
2021-03-09 19:26:39 +0530

    Add files via upload

---
+++ ASP.Net Core/ITOrg2/ASPWebAppITOrg/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPWebAppITOrg.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ITOrganizations()
+        {
+
+            return View();
+        }
+    }
+}
