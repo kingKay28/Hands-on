9824bf9a5d3e4090bdd7e99b5aa20ab62c8f01d6
Kingshuk Pal <44269603+kingKay28@users.noreply.github.com>
2021-03-17 20:24:00 +0530

    Add files via upload

---
+++ Design Principles and Patterns/Abstract factory/AbstractFactAutoMob/AbsFactory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactAutoMob
+{
+    public abstract class Factory
+    {
+        public abstract HeadLight makeHeadLight();
+        public abstract Tire makeTire();
+    }
+    public class Tire
+    {
+
+    }
+    public class HeadLight
+    {
+
+    }
+    public class AudiFactory : Factory
+    {
+        public override HeadLight makeHeadLight() 
+        {
+            HeadLight headLight = new HeadLight();
+            Console.WriteLine("This produces HeadLight for Audi.");
+            return headLight;
+        }
+        public override Tire makeTire()
+        {
+            Tire tire = new Tire();
+            Console.WriteLine("This produces Tire for Audi.");
+            return tire;
+        }
+    }
+
+    public class MercedesFactory : Factory
+    {
+        public override HeadLight makeHeadLight()
+        {
+            HeadLight headLight = new HeadLight();
+            Console.WriteLine("This produces HeadLight for Merecedes.");
+            return headLight;
+        }
+        public override Tire makeTire()
+        {
+            Tire tire = new Tire();
+            Console.WriteLine("This produces Tire for Merecedes.");
+            return tire;
+        }
+    }
+
+    public class AutoFactory
+    {
+        public Factory GetFactory(string factoryType)
+        {
+            if (factoryType == null)
+            {
+                return null;
+            }
+            else if(factoryType.Equals("Audi", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return (new AudiFactory());
+            }
+            else if (factoryType.Equals("Mercedes", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return (new MercedesFactory());
+            }
+            return null;
+        }
+    }
+}
---
+++ Design Principles and Patterns/Abstract factory/AbstractFactAutoMob/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AbstractFactAutoMob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            AutoFactory autoFactory = new AutoFactory();
+            Factory car1 = autoFactory.GetFactory("Audi");
+            Factory car2 = autoFactory.GetFactory("Mercedes");
+            car1.makeHeadLight();
+            car1.makeTire();
+            car2.makeHeadLight();
+            car2.makeTire();
+            Console.ReadLine();
+        }
+    }
+}
