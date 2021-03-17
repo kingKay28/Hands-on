0d6fba949cc8bcf2b8228a9e97fc73d5d28d0788
Kingshuk Pal <44269603+kingKay28@users.noreply.github.com>
2021-03-18 00:05:19 +0530

    Add files via upload

---
+++ Design Principles and Patterns/Adapter Pattern/AdapterPatternCar/Adapter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterPatternCar
+{
+    public interface Movable
+    {
+        double Speed();
+        double Price();
+    }
+
+    public class BugattiVeyron : Movable
+    {
+        //Implementation class for Movable
+        Movable Bugatti;
+         public double Speed() 
+        { 
+            return 268; 
+        }
+
+        public double Price()
+        {
+            return 900000;
+        }
+
+    }
+
+    public interface MovableAdapter
+    {
+        double Speed();
+        double Price();
+    }
+    
+    public class AdaptedBugatti : MovableAdapter
+    {
+
+        // Implementation class for MovableAdapter
+        BugattiVeyron bugattiVeyron = new BugattiVeyron();
+        public double Speed()
+        {
+            
+            return ConvertMPHtoKMPH(bugattiVeyron.Speed());
+        }
+
+        private double ConvertMPHtoKMPH(double MPHSpeed)
+        {
+            return MPHSpeed * 1.60934;
+        }
+
+        public double Price()
+        {
+            return ConvertUSDtoEuro(bugattiVeyron.Price());
+        }
+        public double ConvertUSDtoEuro(double USDPrice)
+        {
+            return USDPrice * 0.84;
+        }
+    }
+class Adapter
+    {
+    }
+}
---
+++ Design Principles and Patterns/Adapter Pattern/AdapterPatternCar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AdapterPatternCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Movable bugatti = new BugattiVeyron();
+            MovableAdapter bugattiAdapted = new AdaptedBugatti();
+            Console.WriteLine("Speed in MPH : "+bugatti.Speed());
+            Console.WriteLine("Speed in KMPH : "+bugattiAdapted.Speed());
+            Console.WriteLine("MPH speed * 1.60934 : " + bugatti.Speed() * 1.60934);
+            Console.WriteLine("Price in USD : " + bugatti.Price());
+            Console.WriteLine("Price in Euro : " + bugattiAdapted.Price());
+            Console.WriteLine("Price in USD * 0.84 : " + bugatti.Price() * 0.84);
+            Console.ReadLine();
+        }
+    }
+}
