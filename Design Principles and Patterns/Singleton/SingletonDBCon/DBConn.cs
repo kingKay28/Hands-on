aa2fc44f85b68897508c6290dfb5f8b2911d76e1
Kingshuk Pal <44269603+kingKay28@users.noreply.github.com>
2021-03-17 20:21:00 +0530

    Add files via upload

---
+++ Design Principles and Patterns/Singleton/SingletonDBCon/DBConn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDBCon
+{
+    class DBConn
+    {
+        private static DBConn instance;
+        private int count = 0;
+
+        //restricting to create multiple instances using "new" keyword 
+        private DBConn()
+        {
+            count++;
+            Console.WriteLine("Count from DBconn constructor : " + count);
+            //code to create data base connection
+        }
+
+        private static object syncLock = new object();
+        public static DBConn Instance
+        {
+            get{
+                lock (syncLock)       
+                {
+                    if (DBConn.instance == null)
+                        DBConn.instance = new DBConn();
+                    return DBConn.instance;
+                }
+            
+            }
+        }
+        
+        public void CallerMethod(int i)
+        {
+             //int i = 0;
+            Console.WriteLine("Counter is at " + i + " times");
+
+        }
+    }
+}
---
+++ Design Principles and Patterns/Singleton/SingletonDBCon/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SingletonDBCon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Console.WriteLine("Hello World!");
+            DBConn conn1 = DBConn.Instance;
+            DBConn conn2 = DBConn.Instance;
+            conn1.CallerMethod(i++);
+            conn2.CallerMethod(i++);
+            Console.ReadLine();
+        }
+    }
+}
